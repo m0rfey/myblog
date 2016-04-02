eb501580fdcd828bf46bf5a87cf6a4647f19b4f8
maximchuk <maksimchuk.mm@gmail.com>
2016-04-02 08:55:42 +0300

    install virtalenv

--- loginsys/models.py
+++ loginsys/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User, UserManager
 from django.db import models
 
 class UserProfile(User):
-    user_id = models.ForeignKey(User)
+    users_id = models.ForeignKey(User)
     phone = models.CharField(max_length=13, blank=False)
 
     objects = UserManager()
